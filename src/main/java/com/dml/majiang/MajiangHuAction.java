51ca5eb75b7126aba7de97129af873836be0b1e4
neo <neo@DESKTOP-ITH4FG3>
2018-07-23 23:28:20 +0800

    继续开发

---
+++ src/main/java/com/dml/majiang/MajiangHuAction.java
@@ -0,0 +1,36 @@
+package com.dml.majiang;
+
+import java.nio.ByteBuffer;
+
+public class MajiangHuAction extends MajiangPlayerAction {
+
+	private Hu hu;
+
+	public MajiangHuAction() {
+	}
+
+	public MajiangHuAction(Hu hu) {
+		this.hu = hu;
+	}
+
+	@Override
+	protected void contentToByteBuffer(ByteBuffer bb) throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void fillContentByByteBuffer(ByteBuffer bb) throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Hu getHu() {
+		return hu;
+	}
+
+	public void setHu(Hu hu) {
+		this.hu = hu;
+	}
+
+}
