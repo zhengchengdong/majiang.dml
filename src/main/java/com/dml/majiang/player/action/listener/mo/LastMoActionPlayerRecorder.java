968ce9d96c3b5469c9a9d6471f2d76fada4412de
林少聪 < @PC-20180515PRDG>
2018-08-29 12:43:55 +0800

    最后摸牌玩家记录器

---
+++ src/main/java/com/dml/majiang/player/action/listener/mo/LastMoActionPlayerRecorder.java
@@ -0,0 +1,34 @@
+package com.dml.majiang.player.action.listener.mo;
+
+import com.dml.majiang.ju.Ju;
+import com.dml.majiang.player.action.mo.MajiangMoAction;
+
+/**
+ * 记录最后摸牌的玩家
+ * 
+ * @author lsc
+ *
+ */
+public class LastMoActionPlayerRecorder implements MajiangPlayerMoActionStatisticsListener {
+
+	private String lastMoActionPlayerId;
+
+	@Override
+	public void updateForNextPan() {
+		lastMoActionPlayerId = null;
+	}
+
+	@Override
+	public void update(MajiangMoAction moAction, Ju ju) throws Exception {
+		lastMoActionPlayerId = moAction.getActionPlayerId();
+	}
+
+	public String getLastMoActionPlayerId() {
+		return lastMoActionPlayerId;
+	}
+
+	public void setLastMoActionPlayerId(String lastMoActionPlayerId) {
+		this.lastMoActionPlayerId = lastMoActionPlayerId;
+	}
+
+}
