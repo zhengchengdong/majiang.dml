409b0ae2975c306b72fce60d0faa5600ecadef8c
林少聪 < @PC-20180515PRDG>
2018-09-07 15:37:24 +0800

    连庄统计

--- src/main/java/com/dml/majiang/player/menfeng/ZhuangXiajiaIsDongIfZhuangNotHuPlayersMenFengDeterminer.java
+++ src/main/java/com/dml/majiang/player/menfeng/ZhuangXiajiaIsDongIfZhuangNotHuPlayersMenFengDeterminer.java
@@ -1,6 +1,8 @@
 package com.dml.majiang.player.menfeng;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.dml.majiang.ju.Ju;
 import com.dml.majiang.pan.Pan;
@@ -9,12 +11,14 @@ import com.dml.majiang.position.MajiangPosition;
 import com.dml.majiang.position.MajiangPositionUtil;
 
 /**
- * 如果上一盘庄没有胡的话，庄的下下家变成东，其他所有玩家顺移。否则不动
+ * 如果上一盘庄没有胡的话，庄的下家变成东，其他所有玩家顺移。否则不动
  * 
  * @author Neo
  *
  */
 public class ZhuangXiajiaIsDongIfZhuangNotHuPlayersMenFengDeterminer implements PlayersMenFengDeterminer {
+
+	private Map<String, Integer> playerLianZhuangCountMap = new HashMap<>();
 
 	@Override
 	public void determinePlayersMenFeng(Ju ju) throws Exception {
@@ -24,6 +28,9 @@ public class ZhuangXiajiaIsDongIfZhuangNotHuPlayersMenFengDeterminer implements 
 		if (!latestFinishedPanResult.ifPlayerHu(zhuangPlayerId)) {// 庄没有胡
 			// 先找出庄的下家
 			String zhuangXiajiaPlayerId = latestFinishedPanResult.findXiajiaPlayerId(zhuangPlayerId);
+			// 下家连庄次数为一
+			playerLianZhuangCountMap = new HashMap<>();
+			playerLianZhuangCountMap.put(zhuangXiajiaPlayerId, 1);
 			// 再计算要顺时针移几步到东
 			MajiangPosition p = latestFinishedPanResult.playerMenFeng(zhuangXiajiaPlayerId);
 			int n = 0;
@@ -47,6 +54,13 @@ public class ZhuangXiajiaIsDongIfZhuangNotHuPlayersMenFengDeterminer implements 
 				currentPan.updatePlayerMenFeng(playerId, newPlayerMenFeng);
 			}
 		} else {
+			// 连庄次数加一
+			Integer lianZhuangCount = playerLianZhuangCountMap.get(zhuangPlayerId);
+			if (lianZhuangCount == null) {// 第一盘map为空
+				playerLianZhuangCountMap.put(zhuangPlayerId, 2);
+			} else {
+				playerLianZhuangCountMap.put(zhuangPlayerId, lianZhuangCount + 1);
+			}
 			List<String> allPlayerIds = latestFinishedPanResult.allPlayerIds();
 			for (String playerId : allPlayerIds) {
 				MajiangPosition playerMenFeng = latestFinishedPanResult.playerMenFeng(playerId);
