cdfe1747e651ce470557c7483916893a9b7c2d80
林少聪 < @PC-20180515PRDG>
2018-09-17 21:41:29 +0800

    胡了之后清除胡牌动作

--- src/main/java/com/dml/majiang/player/action/hu/PlayerSetHuHuActionProcessor.java
+++ src/main/java/com/dml/majiang/player/action/hu/PlayerSetHuHuActionProcessor.java
@@ -13,6 +13,7 @@ public class PlayerSetHuHuActionProcessor implements MajiangPlayerHuActionProces
 		Pan currentPan = ju.getCurrentPan();
 		MajiangPlayer huPlayer = currentPan.findPlayerById(action.getActionPlayerId());
 		huPlayer.setHu(hu);
+		huPlayer.clearActionCandidates();
 	}
 
 }
